c74faaa0c2464f7828ab213ff7adafbd4b1597a3
vagrant <vagrant@vagrant.vm>
2018-09-22 21:20:06 +0000

    added more class to data model

--- model.py
+++ model.py
@@ -11,19 +11,35 @@ class User(db.Model):
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     contributor = db.Column(db.Boolean, nullable=True)
     consumer = db.Column(db.Boolean, nullable=True)
-    username = db.Column(db.String(64), nullable=False)
-    password = db.Column(db.String(64), nullable=False, unique=True)
-    email = db.Column(db.String(64), nullable=False, unique=True)
+    username = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(256), nullable=False, unique=True)
+    email = db.Column(db.String(128), nullable=False, unique=True)
 
 
-class Cars(db.Model):
+class Car(db.Model):
 	"""Cars."""
 
 	__tablename__ = "cars"
 
 	car_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-	# location = 
+	location = db.Column(db.String(256), nullable=False)
 	locked = db.Column(db.Boolean, nullable=False)
+
+
+class Category(db.Model):
+	"""Categories."""
+
+	__tablename__ = "categories"
+
+	category = db.Column(db.String(64), primary_key=True)
+
+
+class Donatable(db.Model):
+	"""Donatables."""
+
+	__tablename__ = "donatables"
+
+	name = db.Column(db.String(64), primary_key=True)
 
 
 class Items(db.Model):
@@ -32,5 +48,36 @@ class Items(db.Model):
 	__tablename__ = "items"
 
 	item_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-	category = db.Column(db.String(64), nullable=False)
+	name = db.Columnn(db.String(64), db.ForeignKey('donatables.donatable'))
+	category = db.Column(db.String(64), db.ForeignKey('categories.category'), nullable=False)
 	car_id = db.Column(db.Integer, db.ForeignKey('cars.car_id'))
+	donator_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+	consumer_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=True)
+	date_given = db.Column(db.DateTime, nullable=False)
+	date_taken = db.Column(db.DateTime, nullable=True)
+
+
+	# donator = db.relationship('User', backref=)
+
+
+
+##############################################################################
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+
+    from server import app
+    connect_to_db(app)
+    db.create_all()
+    print "Connected to DB."
+
+
+
