0d04a194f9dd9b82366afb62245ae187cacf6fe8
E.J. Bryant <ejbryant28@gmail.com>
2018-09-22 21:17:19 +0000

    working on login function

--- server.py
+++ server.py
@@ -3,6 +3,46 @@ app = Flask(__name__)
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
 
+@app.before_request
+def before_request():
+	pass
+
+@app.route('/login', methods=["POST"])
+def login():
+	username = request.form.get('username')
+    username = username.lower()
+
+    password = request.form.get('password')
+
+    user = User.query.filter(User.username==username).first()
+
+    if user is None:
+
+    	flash("Ooops. Try entering again.")
+        return redirect('/login')
+
+    elif user.password == password:
+    	flash("You're logged in")
+    	return redirect('/login')
+
+    else:
+    	flash("Password doesn't match. Try again")
+    	return redirect('/upload-potential-item')
+
+
+@app.route('/login-with-face', methods=["POST"])
+def login-face():
+	pass
+
+@app.route('/upload-potential-item', methods=["POST"])
+def upload-potential-item():
+	pass
+
+@app.route('/inventory', methods=["GET"])
+def inventory():
+	pass
+
+
 
 
 
